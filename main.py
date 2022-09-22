b78f9eec5125250dba16981f6b3b52d781233758
Katherine Abrikian <kabrikian@gmail.com>
2022-09-22 13:56:22 -0400

    Resolved same changes again to accept Galina's changes

--- main.py
+++ main.py
@@ -2,16 +2,11 @@ import pytest
 
 
 def always_returns_true():
-<<<<<<< HEAD
     x = 2
     if x < 5:
         return True
     else:
         return False
-=======
-    return None
->>>>>>> 74141686ac15fcae9d3912fe26b7479b29ef6688
-
 
 def test_always_returns_true():
     assert always_returns_true()
